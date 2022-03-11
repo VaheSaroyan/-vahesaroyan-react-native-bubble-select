ac8db730199913d94bc9009931c8a9d1b8ed2fc7
Vahe Saroyan <vahe.saroyan.web@gmail.com>
2022-03-11 15:11:29 +0400

    Fix bubble issue

--- ios/Magnetic/Magnetic.swift
+++ ios/Magnetic/Magnetic.swift
@@ -15,7 +15,7 @@ import SpriteKit
 }
 
 @objcMembers open class Magnetic: SKScene {
-    
+
     /**
      The field node that accelerates the nodes.
      */
@@ -24,76 +24,76 @@ import SpriteKit
         self.addChild(field)
         return field
     }()
-    
+
     /**
      Controls whether you can select multiple nodes.
      */
     open var allowsMultipleSelection: Bool = true
-    
-    
+
+
     /**
     Controls whether an item can be removed by holding down
      */
     open var removeNodeOnLongPress: Bool = false
-    
+
     /**
      The length of time (in seconds) the node must be held on to trigger a remove event
      */
     open var longPressDuration: TimeInterval = 0.35
-    
+
     open var isDragging: Bool = false
-    
+
     /**
      The selected children.
      */
     open var selectedChildren: [Node] {
         return children.compactMap { $0 as? Node }.filter { $0.isSelected }
     }
-    
+
     /**
      The removed children.
      */
     open var removedChildren: [Node] {
         return children.compactMap { $0 as? Node }.filter { $0.isRemoved }
     }
-    
+
     /**
      The object that acts as the delegate of the scene.
-     
+
      The delegate must adopt the MagneticDelegate protocol. The delegate is not retained.
      */
     open weak var magneticDelegate: MagneticDelegate?
-    
+
     private var touchStarted: TimeInterval?
-    
+
     override open var size: CGSize {
         didSet {
             configure()
         }
     }
-    
+
     override public init(size: CGSize) {
         super.init(size: size)
-        
+
         commonInit()
     }
-    
+
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
+
         commonInit()
     }
-    
+
     func commonInit() {
         backgroundColor = .white
         scaleMode = .aspectFill
         configure()
     }
-    
+
     func configure() {
         let strength = Float(max(size.width, size.height))
         let radius = strength.squareRoot() * 100
-        
+
         physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         physicsBody = SKPhysicsBody(edgeLoopFrom: { () -> CGRect in
             var frame = self.frame
@@ -101,13 +101,13 @@ import SpriteKit
             frame.origin.x -= frame.size.width / 2
             return frame
         }())
-        
+
         magneticField.region = SKRegion(radius: radius)
         magneticField.minimumRadius = radius
         magneticField.strength = strength
         magneticField.position = CGPoint(x: size.width / 2, y: size.height / 2)
     }
-    
+
     override open func addChild(_ node: SKNode) {
         var x = -node.frame.width // left
         if children.count % 2 == 0 {
@@ -117,39 +117,39 @@ import SpriteKit
         node.position = CGPoint(x: x, y: y)
         super.addChild(node)
     }
-    
+
 }
 
 extension Magnetic {
-    
+
     open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard removeNodeOnLongPress, let touch = touches.first else { return }
         touchStarted = touch.timestamp
     }
-    
+
     override open func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
         let previous = touch.previousLocation(in: self)
         guard location.distance(from: previous) != 0 else { return }
-        
+
         isDragging = true
-        
+
         moveNodes(location: location, previous: previous)
     }
-    
+
     override open func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
-        
+
         defer { isDragging = false }
         guard !isDragging, let node = node(at: location) else { return }
-                
+
         if removeNodeOnLongPress && !node.isSelected {
             guard let touchStarted = touchStarted else { return }
             let touchEnded = touch.timestamp
             let timeDiff = touchEnded - touchStarted
-            
+
             if (timeDiff >= longPressDuration) {
                 node.removedAnimation {
                     node.isRemoved = true
@@ -158,7 +158,7 @@ extension Magnetic {
                 return
             }
         }
-        
+
         if node.isSelected {
             node.isSelected = false
             magneticDelegate?.magnetic(self, didDeselect: node)
@@ -167,23 +167,26 @@ extension Magnetic {
                 selectedNode.isSelected = false
                 magneticDelegate?.magnetic(self, didDeselect: selectedNode)
             }
-            node.isSelected = true
-            magneticDelegate?.magnetic(self, didSelect: node)
+            if node.id != "" {
+                node.isSelected = true
+                magneticDelegate?.magnetic(self, didSelect: node)
+            }
+
         }
     }
-    
+
     override open func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         isDragging = false
     }
-    
+
 }
 
 extension Magnetic {
-    
+
     open func moveNodes(location: CGPoint, previous: CGPoint) {
         let x = location.x - previous.x
         let y = location.y - previous.y
-        
+
         for node in children {
             let distance = node.position.distance(from: location)
             let acceleration: CGFloat = 3 * pow(distance, 1/2)
@@ -191,9 +194,9 @@ extension Magnetic {
             node.physicsBody?.applyForce(direction)
         }
     }
-    
+
     open func node(at point: CGPoint) -> Node? {
         return nodes(at: point).compactMap { $0 as? Node }.filter { $0.path!.contains(convert(point, to: $0)) }.first
     }
-    
+
 }
